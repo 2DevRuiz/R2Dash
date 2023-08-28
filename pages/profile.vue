92fe4341145809f55fa0af81b5e02c0274f2b6b5
Darwing Ruiz <darjruiz11@gmail.com>
2023-08-28 16:53:42 -0600

    input

--- pages/profile.vue
+++ pages/profile.vue
@@ -10,14 +10,14 @@
                         <div class="">
                             <div class="relative mb-6">
                                 <input id="txt_username" name="txt_username" type="text" 
-                                    class="peer/username p-2 pe-10 h-10 mt-1 w-full rounded-md border-b-2 bg-[#BDBCBD] bg-opacity-50 border-gray-300 text-gray-50 placeholder-transparent focus:outline-none focus:border-[#00BBE1] invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+                                    class="peer/username p-2 pe-10 h-10 mt-1 w-full rounded-md border-b-2  border-gray-300 text-gray-500 placeholder-transparent focus:outline-none focus:border-[#00BBE1] invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
                                     placeholder="Username" />
                                 <label for="txt_username"
-                                    class="absolute left-2 -top-4  text-white text-sm transition-all peer-placeholder-shown/username:text-base peer-placeholder-shown/username:text-white peer-placeholder-shown/username:top-3 peer-focus/username:-top-4 peer-focus/username:text-[#00BBE1] peer-focus/username:text-sm">Username</label>
-                                <span
+                                    class="absolute left-2 -top-4  text-gray-500 text-sm transition-all peer-placeholder-shown/username:text-base peer-placeholder-shown/username:text-gray-500 peer-placeholder-shown/username:top-3 peer-focus/username:-top-4 peer-focus/username:text-[#00BBE1] peer-focus/username:text-sm">Username</label>
+                                <!-- <span
                                     class="pointer-events-none absolute inset-y-0 end-0 grid w-10 place-content-center align-middle text-gray-300 peer-focus/username:text-[#00BBE1]">
                                     <font-awesome-icon :icon="['fas', 'user']" class="text-lg  " />
-                                </span>
+                                </span> -->
                             </div>
                         </div>
                         <div>02</div>
