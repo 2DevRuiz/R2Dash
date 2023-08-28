63a98fd14e53fb0a851c391e2dc58e3d483221fc
Darwing Ruiz <darjruiz11@gmail.com>
2023-08-28 16:57:50 -0600

    input2

--- pages/profile.vue
+++ pages/profile.vue
@@ -10,7 +10,7 @@
                         <div class="">
                             <div class="relative mb-6">
                                 <input id="txt_username" name="txt_username" type="text" 
-                                    class="peer/username p-2 pe-10 h-10 mt-1 w-full rounded-md border-b-2  border-gray-300 text-gray-500 placeholder-transparent focus:outline-none focus:border-[#00BBE1] invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
+                                    class="peer/username p-2 pe-10 h-10 mt-1 w-full rounded-md border-b-2 border-gray-300 text-gray-500 placeholder-transparent focus:outline-none focus:border-[#00BBE1] invalid:[&:not(:placeholder-shown):not(:focus)]:border-red-500"
                                     placeholder="Username" />
                                 <label for="txt_username"
                                     class="absolute left-2 -top-4  text-gray-500 text-sm transition-all peer-placeholder-shown/username:text-base peer-placeholder-shown/username:text-gray-500 peer-placeholder-shown/username:top-3 peer-focus/username:-top-4 peer-focus/username:text-[#00BBE1] peer-focus/username:text-sm">Username</label>
